bbbe0811446db788dffdf874d0894f348f22e100
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-04-26 23:18:25 +0800

    backend

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Budget Tracker</h1>
+      <p>Welcome to your Budget Tracking Application!</p>
+    </div>
+  );
+}
+
+export default App; 
